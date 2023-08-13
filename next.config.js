1e2d7dad57a33bf7db8f5ca1e37bc7d09a7eb978
신용준 <shinyj1991@gmail.com>
2023-08-13 19:18:21 +0900

    cicd : output export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  compiler: {
+    styledComponents: {
+      ssr: true, // SSR을 위한 설정
+      displayName: true, // 클래스명에 컴포넌트 이름을 붙임
+      pure: true, // dead code elimination (사용되지 않는 속성 제거)
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
